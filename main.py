dc79a5f4fe01a99beb2c966222633de924477bf2
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-09-28 10:46:57 +0530

    got tts and sr working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr
+import pyttsx3 
 
 r = sr.Recognizer()
 
@@ -6,9 +7,28 @@ with sr.Microphone() as source:
     print("Say Something")
     audio = r.listen(source)
 
-try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
-except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
+def SpeakText(command):
+    
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command) 
+    engine.runAndWait()
+
+while(1):    
+    try:
+        with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source, duration=0.5)
+             
+            audio = r.listen(source)
+            
+            MyText = r.recognize_google(audio)
+            MyText = MyText.lower()
+
+            print(f"Did you say ,{MyText}")
+            SpeakText(f"Did you say ,{MyText}")
+            
+    except sr.RequestError as e:
+        print(f"Could not request results: {e}")
+        
+    except sr.UnknownValueError as e:
+        print(f"Could not get value: {e}")
