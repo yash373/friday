2551d3717ab0c24837effa59d9efc3c857424d2d
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-10-01 19:48:03 +0530

    got ai part working

---
+++ research.py
@@ -0,0 +1,28 @@
+import requests
+
+def generateText(text:str) -> str:
+    url = "https://chatgpt-42.p.rapidapi.com/conversationgpt4-2"
+
+    payload = {
+        "messages": [
+            {
+                "role": "user",
+                "content": "who is the pm of india"
+            }
+        ],
+        "system_prompt": "",
+        "temperature": 0.9,
+        "top_k": 5,
+        "top_p": 0.9,
+        "max_tokens": 300,
+        "web_access": False
+    }
+    headers = {
+        "x-rapidapi-key": "2010442decmshc62de9e7247e8e1p15b96cjsn1cf015d3184b",
+        "x-rapidapi-host": "chatgpt-42.p.rapidapi.com",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    return (response.json()["result"])
